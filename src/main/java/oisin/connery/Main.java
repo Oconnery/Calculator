9066f7185b6c314654f7e81bd4580a358091dc8c
Oisin <oisinconnery@hotmail.co.uk>
2023-08-02 07:44:43 +0100

    JX-12: Add user input

--- src/main/java/oisin/connery/Main.java
+++ src/main/java/oisin/connery/Main.java
@@ -1,7 +1,12 @@
 package oisin.connery;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // todo scanner/writer
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter expression: ");
+        String answer = Computer.calculate(scanner.nextLine());
+        System.out.println(answer);
     }
 }
